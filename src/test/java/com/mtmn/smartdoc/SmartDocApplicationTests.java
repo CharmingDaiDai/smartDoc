02eb686018060fc69051c700e435be69c3007ca4
charmingdaidai <1179121958@qq.com>
2025-04-15 14:58:25 +0800

    first commit

---
+++ src/test/java/com/mtmn/smartdoc/SmartDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtmn.smartdoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
