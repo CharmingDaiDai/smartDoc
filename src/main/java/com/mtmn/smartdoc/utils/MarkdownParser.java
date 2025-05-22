b4e68225bc446c9aed7314bfc1e2fbeefda393e1
charmingdaidai <1179121958@qq.com>
2025-05-22 10:49:30 +0800

    新增 MyNode 和 MarkdownParser 类的初始实现

---
+++ src/main/java/com/mtmn/smartdoc/utils/MarkdownParser.java
@@ -0,0 +1,10 @@
+package com.mtmn.smartdoc.utils;
+
+/**
+ * @author charmingdaidai
+ * @version 1.0
+ * @description  TODO
+ * @date 2025/5/22 10:38
+ */
+public class MarkdownParser {
+}
---
+++ src/test/java/com/mtmn/smartdoc/utils/MarkdownParserTest.java
@@ -0,0 +1,10 @@
+package com.mtmn.smartdoc.utils;
+
+/**
+ * @author charmingdaidai
+ * @version 1.0
+ * @description  TODO
+ * @date 2025/5/22 10:40
+ */
+public class MarkdownParserTest {
+}
