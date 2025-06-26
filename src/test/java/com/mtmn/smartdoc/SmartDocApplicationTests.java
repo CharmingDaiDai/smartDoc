5d0e9990362384bfa4364ba01c01c0d80be84c5e
charmingdaidai <1179121958@qq.com>
2025-06-26 17:18:22 +0800

    。

--- src/test/java/com/mtmn/smartdoc/SmartDocApplicationTests.java
+++ src/test/java/com/mtmn/smartdoc/SmartDocApplicationTests.java
@@ -3,6 +3,7 @@ package com.mtmn.smartdoc;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -10,28 +11,34 @@ import java.util.concurrent.Executors;
 @SpringBootTest
 class SmartDocApplicationTests {
 
-    @Test
-    void contextLoads() {
+  @Test
+  void contextLoads() {
+  }
+
+  public static void main(String[] args) {
+    class Solution {
+      public int[][] divideArray(int[] nums, int k) {
+        int n = nums.length, len = n / 3;
+        List<List<Integer>> res = new ArrayList<>();
+        if (n < 3)
+          return new int[0][];
+        Arrays.sort(nums);
+
+        int t = 0, ff = nums[0] + k;
+        for (int i = 0; i < n; i += len) {
+          if(i + len - 1 < n && nums[i] + k < nums[i + len - 1]){
+            return new int[0][];
+          }
+          int tt = len;
+          while(tt-- > 0){
+            res.get(t).add(nums[i]);
+            i++;
+          }
+        }
+
+        return (int[][])res.toArray();
+      }
     }
-
-    public static void main(String[] args) {
-        ExecutorService executor = Executors.newFixedThreadPool(5);
-
-        CompletableFuture<String> cf1 = CompletableFuture.supplyAsync(() -> {
-            System.out.println("执行step 1");
-            return "step1 result";
-        }, executor);
-
-        CompletableFuture<String> cf2 = CompletableFuture.supplyAsync(() -> {
-            System.out.println("执行step 2");
-            return "step2 result";
-        });
-
-        cf1.thenCombine(cf2, (result1, result2) -> {
-            System.out.println(result1 + " , " + result2);
-            System.out.println("执行step 3");
-            return "step3 result";
-        }).thenAccept(System.out::println);
-    }
+  }
 
 }
