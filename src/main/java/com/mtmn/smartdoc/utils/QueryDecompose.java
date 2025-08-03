678e4fa2fbff91451b8bce653cc5c16954ee681c
charmingdaidai <1179121958@qq.com>
2025-08-03 15:07:24 +0800

    feat: 添加查询重写器类及其配置，支持复杂问题的分解和重写

---
+++ src/main/java/com/mtmn/smartdoc/utils/QueryDecompose.java
@@ -0,0 +1,48 @@
+package com.mtmn.smartdoc.utils;
+
+import com.mtmn.smartdoc.common.QueryRewriteResult;
+import com.mtmn.smartdoc.service.LLMService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author charmingdaidai
+ * @version 1.0
+ * @description TODO
+ * @date 2025/6/1 13:23
+ */
+@Service
+@Log4j2
+public class QueryRewrite {
+
+    @Autowired
+    private QueryRewriteParser queryRewriteParser;
+
+    @Autowired
+    private LLMService llmService;
+
+    @Value("${prompt.queryRewrite}")
+    private static String SIMPLE_REWRITE_PROMPT;
+
+    public QueryRewriteResult rewriteQuery(String conversationHistory, String originalQuery) {
+        try {
+            String prompt = String.format(SIMPLE_REWRITE_PROMPT, conversationHistory, originalQuery);
+
+            String response = llmService.createChatModel().chat(prompt);
+            QueryRewriteResult result = queryRewriteParser.parseSimpleResponse(response);
+            result.setOriginalQuery(originalQuery);
+
+            // 记录日志
+            log.info("查询重写 - 原始: {} | 重写: {} | 原因: {}",
+                    originalQuery, result.getRewrittenQuery(), result.getReason());
+
+            return result;
+
+        } catch (Exception e) {
+            log.error("查询重写失败", e);
+            return QueryRewriteResult.builder().rewrittenQuery(originalQuery).build();
+        }
+    }
+}
